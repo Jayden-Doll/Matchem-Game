88412cee53a88fccdc1bd59db746c4538ba85620
Jayden Doll <jayjdoll002@gmail.com>
2022-04-05 14:11:25 -0700

    Add card amount selector

--- script.js
+++ script.js
@@ -8,199 +8,246 @@ const overlay = document.querySelector("#overlay");
 const message = document.querySelector("#popup-message");
 const restartButton = document.querySelector("#btn-restart");
 const toMenuButton = document.querySelector("#btn-to-menu");
-
-startButton.addEventListener("click", () => {
-  startScreen.classList.remove("menu-show");
-  startScreen.classList.add("menu-hide");
-});
-
-attemptCount.innerText = 5;
+const cardAmountButtons = document.querySelectorAll(".btn-option");
+const buttonOption12 = document.querySelector("#btn-12");
+const buttonOption24 = document.querySelector("#btn-24");
+const buttonOption36 = document.querySelector("#btn-36");
+let smBoard = 12;
+let mdBoard = 24;
+let lgBoard = 36;
 
 //Amount of cards to be created
-let cardCount = 12;
-//Array of possible id numbers for matching the cards
-let numArray = [];
 
-//Generates 2 sets of matching numbers depending on the cardCount
-function generateNumArray(cardCount) {
-  cardCount /= 2;
-  for (i = 1; i < cardCount + 1; i++) {
-    numArray.push(i);
-    numArray.push(i);
+function runGame(cardCount) {
+  startButton.addEventListener("click", () => {
+    startScreen.classList.remove("menu-show");
+    startScreen.classList.add("menu-hide");
+  });
+
+  //Array of possible id numbers for matching the cards
+  let numArray = [];
+
+  //Generates 2 sets of matching numbers depending on the cardCount
+  function generateNumArray(cardCount) {
+    cardCount /= 2;
+    for (i = 1; i < cardCount + 1; i++) {
+      numArray.push(i);
+      numArray.push(i);
+    }
   }
-}
 
-generateNumArray(cardCount);
+  generateNumArray(cardCount);
 
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * i);
-    let k = array[i];
-    array[i] = array[j];
-    array[j] = k;
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * i);
+      let k = array[i];
+      array[i] = array[j];
+      array[j] = k;
+    }
   }
-}
 
-shuffleArray(numArray);
+  shuffleArray(numArray);
 
-function createCard(cardCount) {
-  for (let i = 0; i < cardCount; i++) {
-    const cardBody = document.createElement("div");
-    const innerCard = document.createElement("div");
-    const cardFront = document.createElement("div");
-    const cardBack = document.createElement("div");
-    const cardImage = document.createElement("img");
+  function createCard(cardCount) {
+    const gridContainer = document.createElement("div");
+    for (let i = 0; i < cardCount; i++) {
+      const cardBody = document.createElement("div");
+      const innerCard = document.createElement("div");
+      const cardFront = document.createElement("div");
+      const cardBack = document.createElement("div");
+      const cardImage = document.createElement("img");
 
-    gridContainer.appendChild(cardBody).classList.add("grid-card");
-    cardBody.appendChild(innerCard).classList.add("card-inner");
-    innerCard.appendChild(cardFront).classList.add("card-front");
-    innerCard.appendChild(cardBack).classList.add("card-back");
-    cardBack.appendChild(cardImage);
+      document.body
+        .appendChild(gridContainer)
+        .classList.add("grid-container", "grid");
+      cardCount === smBoard ? gridContainer.classList.add("grid-12") : false;
+      cardCount === mdBoard ? gridContainer.classList.add("grid-24") : false;
+      cardCount === lgBoard ? gridContainer.classList.add("grid-36") : false;
+      gridContainer.appendChild(cardBody).classList.add("grid-card");
+      cardBody.appendChild(innerCard).classList.add("card-inner");
+      innerCard.appendChild(cardFront).classList.add("card-front");
+      innerCard.appendChild(cardBack).classList.add("card-back");
+      cardBack.appendChild(cardImage);
 
-    const randNum = numArray.pop();
+      const randNum = numArray.pop();
 
-    innerCard.id = randNum;
-    cardImage.src = `./imgs/${randNum}.png`;
+      innerCard.id = randNum;
+      cardImage.src = `./imgs/${randNum}.png`;
+    }
   }
-}
 
-createCard(cardCount);
+  createCard(cardCount);
 
-const cardGrid = document.querySelectorAll(".card-inner");
+  const cardGrid = document.querySelectorAll(".card-inner");
 
-//Card ID and selected cards arrays
-let cardID = [];
-let selectedCards = [];
+  //Card ID and selected cards arrays
+  let cardID = [];
+  let selectedCards = [];
 
-let attempts = 5;
-let matches = 0;
+  let attempts;
+  cardCount === smBoard ? (attempts = 5) : false;
+  cardCount === mdBoard ? (attempts = 12) : false;
+  cardCount === lgBoard ? (attempts = 20) : false;
+  attemptCount.innerText = attempts;
+  let matches = 0;
 
-function removeCards() {
-  while (gridContainer.firstChild) {
-    gridContainer.removeChild(gridContainer.firstChild);
+  function removeCards() {
+    while (gridContainer.firstChild) {
+      gridContainer.removeChild(gridContainer.firstChild);
+    }
   }
-}
 
-function resetGame() {
-  popup.classList.add("popup-hidden");
-  overlay.classList.add("overlay-hidden");
+  function resetGame() {
+    popup.classList.add("popup-hidden");
+    overlay.classList.add("overlay-hidden");
 
-  setTimeout(() => {
-    const newCardGrid = document.querySelectorAll(".card-inner");
-    if (newCardGrid) {
-      newCardGrid.forEach((card) => {
+    setTimeout(() => {
+      const newCardGrid = document.querySelectorAll(".card-inner");
+      if (newCardGrid) {
+        newCardGrid.forEach((card) => {
+          card.classList.remove("rotate", "active", "matched", "disabled");
+          card.classList.add("noclick");
+        });
+      }
+
+      cardGrid.forEach((card) => {
         card.classList.remove("rotate", "active", "matched", "disabled");
         card.classList.add("noclick");
       });
-    }
+    }, 400);
 
-    cardGrid.forEach((card) => {
-      card.classList.remove("rotate", "active", "matched", "disabled");
-      card.classList.add("noclick");
-    });
-  }, 400);
+    setTimeout(() => {
+      document.querySelector(".grid-container").remove();
+      cardCount === smBoard ? (attempts = 5) : false;
+      cardCount === mdBoard ? (attempts = 12) : false;
+      cardCount === lgBoard ? (attempts = 20) : false;
+      attemptCount.innerText = attempts;
+      matches = 0;
 
-  setTimeout(() => {
-    attemptCount.innerText = 5;
-    attempts = 5;
-    matches = 0;
+      removeCards();
 
-    removeCards();
+      generateNumArray(cardCount);
+      shuffleArray(numArray);
 
-    generateNumArray(cardCount);
-    shuffleArray(numArray);
+      createCard(cardCount);
 
-    createCard(cardCount);
+      const newCardGrid = document.querySelectorAll(".card-inner");
 
-    const newCardGrid = document.querySelectorAll(".card-inner");
-
-    runGameLogic(newCardGrid);
-  }, 700);
-}
-
-restartButton.addEventListener("click", () => {
-  resetGame();
-});
-
-toMenuButton.addEventListener("click", () => {
-  resetGame();
-  startScreen.classList.remove("menu-hide");
-  startScreen.classList.add("menu-show");
-});
-
-//Pushes selected card object and it's id into the arrays
-
-function runGameLogic(grid) {
-  function selectCard(card) {
-    cardID.push(card.id);
-    selectedCards.push(card);
-
-    card.classList.add("active");
-    card.classList.add("rotate");
+      runGameLogic(newCardGrid);
+    }, 700);
   }
 
-  grid.forEach((card) => {
-    card.addEventListener("click", () => {
-      selectCard(card);
+  restartButton.addEventListener("click", () => {
+    resetGame();
+  });
 
-      //If 2 cards are selected
-      if (cardID.length === 2) {
-        //If both card's ids match
-        if (cardID[0] === cardID[1]) {
-          matches++;
+  toMenuButton.addEventListener("click", () => {
+    resetGame();
+    startScreen.classList.remove("menu-hide");
+    startScreen.classList.add("menu-show");
+  });
 
-          if (matches === cardCount / 2) {
-            message.innerText = "You Win!";
-            message.style.color = "rgb(52, 193, 63)";
-            popup.classList.remove("popup-hidden");
-            overlay.classList.remove("overlay-hidden");
-          }
+  //Pushes selected card object and it's id into the arrays
 
-          for (const cards of selectedCards) {
-            cards.classList.add("matched");
-            cards.classList.remove("active");
+  function runGameLogic(grid) {
+    function selectCard(card) {
+      cardID.push(card.id);
+      selectedCards.push(card);
 
-            //Empty both arrays
-            cardID = [];
-            selectedCards = [];
-          }
-        } else {
-          attempts--;
-          attemptCount.innerText = `${attempts}`;
+      card.classList.add("active");
+      card.classList.add("rotate");
+    }
 
-          if (attempts === 0) {
-            message.innerText = "You Lose.";
-            message.style.color = "rgb(193, 52, 52)";
-            popup.classList.remove("popup-hidden");
-            overlay.classList.remove("overlay-hidden");
+    grid.forEach((card) => {
+      card.addEventListener("click", () => {
+        selectCard(card);
 
-            //Show every card and disable them
-            grid.forEach((poop) => {
-              poop.classList.add("disabled", "rotate");
-              setTimeout(() => {
+        //If 2 cards are selected
+        if (cardID.length === 2) {
+          //If both card's ids match
+          if (cardID[0] === cardID[1]) {
+            matches++;
+
+            if (matches === cardCount / 2) {
+              message.innerText = "You Win!";
+              message.style.color = "rgb(52, 193, 63)";
+              popup.classList.remove("popup-hidden");
+              overlay.classList.remove("overlay-hidden");
+            }
+
+            for (const cards of selectedCards) {
+              cards.classList.add("matched");
+              cards.classList.remove("active");
+
+              //Empty both arrays
+              cardID = [];
+              selectedCards = [];
+            }
+          } else {
+            attempts--;
+            attemptCount.innerText = `${attempts}`;
+
+            if (attempts === 0) {
+              message.innerText = "You Lose.";
+              message.style.color = "rgb(193, 52, 52)";
+              popup.classList.remove("popup-hidden");
+              overlay.classList.remove("overlay-hidden");
+
+              //Show every card and disable them
+              grid.forEach((poop) => {
                 poop.classList.add("disabled", "rotate");
-              }, 1001);
-            });
-          }
+                setTimeout(() => {
+                  poop.classList.add("disabled", "rotate");
+                }, 1001);
+              });
+            }
 
-          //Add disabled class, and rotate cards back after a second
-          for (const cards of selectedCards) {
-            cards.classList.remove("active");
-            cards.classList.add("disabled");
+            //Add disabled class, and rotate cards back after a second
+            for (const cards of selectedCards) {
+              cards.classList.remove("active");
+              cards.classList.add("disabled");
 
-            setTimeout(() => {
-              cards.classList.remove("rotate");
-              cards.classList.remove("disabled");
-            }, 1000);
+              setTimeout(() => {
+                cards.classList.remove("rotate");
+                cards.classList.remove("disabled");
+              }, 1000);
 
-            //Empty both arrays
-            cardID = [];
-            selectedCards = [];
+              //Empty both arrays
+              cardID = [];
+              selectedCards = [];
+            }
           }
         }
-      }
+      });
     });
-  });
+  }
+
+  runGameLogic(cardGrid);
 }
 
-runGameLogic(cardGrid);
+let cardCount;
+buttonOption12.addEventListener("click", () => {
+  const gridContainer = document.querySelector(".grid");
+  if (gridContainer) {
+    gridContainer.remove();
+  }
+  let cardCount = smBoard;
+  runGame(cardCount);
+});
+buttonOption24.addEventListener("click", () => {
+  const gridContainer = document.querySelector(".grid");
+  if (gridContainer) {
+    gridContainer.remove();
+  }
+  let cardCount = mdBoard;
+  runGame(cardCount);
+});
+buttonOption36.addEventListener("click", () => {
+  const gridContainer = document.querySelector(".grid");
+  if (gridContainer) {
+    gridContainer.remove();
+  }
+  let cardCount = lgBoard;
+  runGame(cardCount);
+});
